0494a996dfbcbba4927e7a6c10d78a0879ddbcd6
Konstantin <kastett@mail.ru>
2023-07-18 00:06:56 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.Write("Введите ваше имя");
+// string username = Console.ReadLine();
+// Console.Write("Привет, ");
+// Console.Write(username);
+
+double numberA = 31;
+double numberВ = 5;
+double result1 = numberA + numberВ;
+Console.WriteLine(result1);
+double result2 = numberA / numberВ;
+Console.WriteLine(result2);
