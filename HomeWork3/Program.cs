ae931452ca8bc468f3d079a4a0de6ddf48c88a82
Konstantin <kastett@mail.ru>
2023-08-06 01:12:38 +0300

    Homework4

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,171 @@
+﻿// Задача 19 - Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+// Console.WriteLine("Insert number");
+// int number = Convert.ToInt32(Console.ReadLine());
+// (var a1, var a2, var a3, var a4, var a5) = (Convert.ToString(number)[0], Convert.ToString(number)[1], Convert.ToString(number)[2], Convert.ToString(number)[3], Convert.ToString(number)[4]);
+// if ((a1 == a5) & (a2 == a4)) Console.WriteLine("This NUMBER is PALINDROME");
+// else Console.WriteLine("This NUMBER is not PALINDROME");
+
+// Задача 21 - Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+// Console.WriteLine("Insert coordinat X1,Y1,Z1");
+// double[] n1 = new double[3];
+// for (int i = 0; i < n1.Length; i++)
+// {
+//     n1[i] = Convert.ToDouble(Console.ReadLine());
+// }
+// Console.WriteLine("Insert coordinat X2,Y2,Z2");
+// double[] n2 = new double[3];
+// for (int i = 0; i < n2.Length; i++)
+// {
+//     n2[i] = Convert.ToDouble(Console.ReadLine());
+// }
+// Console.WriteLine("Dimention = " + Math.Round(Math.Sqrt(Math.Pow(n2[0] - n1[0], 2) + Math.Pow(n2[1] - n1[1], 2) + Math.Pow(n2[2] - n1[2], 2)), 2));
+
+// Задача 23 - Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+// int n = Convert.ToInt32(Console.ReadLine());
+// for (int i = 1; i <= n; i++)
+// {
+//     Console.WriteLine(Math.Pow(i, 3));
+// }
+
+
+// Задача 25: 
+// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+// int a = Prompt("Введите основание - ");
+// int b = Prompt("Введите степень - ");
+// int Prompt(string message) // Вводим числа с клавиатуры
+// {
+//     System.Console.Write(message);
+//     int number = Convert.ToInt32(Console.ReadLine());
+//     return number;
+// }
+// bool Zero(int b) // Проверяем меньше нуля
+// {
+//     if (b < 0)
+//     {
+//         System.Console.WriteLine("Показатель не может быть меньше нуля");
+//         return false;
+//     }
+//     else
+//     {
+//         return true;
+//     }
+// }
+// if (Zero(b)) //Выводим расчет
+// {
+//     System.Console.WriteLine($"Число {a} в степени {b} будет равно {Math.Pow(a, b)}");
+// }
+
+// Задача 27: 
+// Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// int a = Prompt("Введите число - ");
+// int Prompt(string message) // Вводим число с клавиатуры
+// {
+//     System.Console.Write(message);
+//     int number = Convert.ToInt32(Console.ReadLine());
+//     return number;
+// }
+// int Summa(int a)
+// {
+//     int summa = 0;
+//     while (a != 0)
+//     {
+//         summa += a % 10;
+//         a = a / 10;
+//     }
+//     return summa;
+// }
+// System.Console.WriteLine("Сумма цифр числа составляет - " + Summa(a));
+
+// Задача 29: 
+// Напишите программу, которая задаёт массив из m элементов 
+// и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+// Console.WriteLine("Введите количество элементов массива: ");//спрашиваем сколько чисел будет
+// int m = Convert.ToInt32(Console.ReadLine());
+// int[] array = new int[m];
+// for (int i = 0; i < m; i++) //Вводим числа массива
+// { array[i] = Prompt($"Введите {i + 1} элемент массива:"); }
+// int Prompt(string message) // Вводим число с клавиатуры
+// {
+//     System.Console.Write(message);
+//     int number = Convert.ToInt32(Console.ReadLine());
+//     return number;
+// }
+// PrintArr(array);//Выводим массив в строку
+// void PrintArr(int[] array)
+// {
+//     System.Console.Write("[");
+//     for (int i = 0; i < m; i++)
+//     {
+//         if (i == (m - 1))
+//         System.Console.Write($"{array[i]}"); 
+//         else
+//         System.Console.Write($"{array[i]}, ");
+//     }
+//     System.Console.Write("]");
+// }
+
+
+// 10.08.2023
+// Задача 34: 
+// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+
+
+// Задача 36: 
+// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+
+
+// Задача 38: 
+// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+
+
+
+// 28.08.2023
+// Задача 41: 
+// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+
+
+// Задача 43: 
+// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
---
+++ Seminar2/Program.cs
@@ -0,0 +1,117 @@
+﻿// Seminar2
+/*
+13254 / 10 = 1325
+13254 / 100 = 132
+13254 / 1000 = 13
+13254 / 10000 = 1
+
+13254 % 10 = 4
+13254 % 100 = 54
+13254 % 1000 = 254
+13254 % 10000 = 3254
+
+24365 -> 43
+*/
+
+// TASK1 Программа которая выводит случайное трехзначное число
+// и удаляет вторую цифру этого числа
+
+/*
+int CutNumber(int num);
+{
+    int hundreds = num / 100;
+    int units = num % 10;
+    return hundreds * 10 + units;
+}
+int randNumber = new Random().Next(100, 1000);
+int newNumber = CutNumber(randNumber);
+Console.WriteLine($"New Version of {randNumber} is {newNumber}");
+*/
+
+/*
+bool IsDivisible(int num, int div1, int div2)
+{
+    // if (num %div1 == 0 &&)
+    return num % div1 == 0 && num % div2 == 0;
+}
+Console.WriteLine("Input a number");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a First DIV1");
+int div1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a First DIV2");
+int div2 = Convert.ToInt32(Console.ReadLine());
+
+bool res = IsDivisible(number, div1, div2);
+if (res) Console.Write("YES");
+else Console.Write("NO");
+*/
+
+// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+/*
+int number = new Random().Next(10, 100);
+Console.WriteLine("Случайное число: " + number);
+int maxDigit = 0;
+int currentDigit = 0;
+while (number > 0)
+{
+    currentDigit = number % 10;
+    if (currentDigit > maxDigit)
+    {
+        maxDigit = currentDigit;
+    }
+    number /= 10;
+}
+Console.WriteLine("Наибольшая цифра числа: " + maxDigit);
+*/
+// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+/*
+Console.WriteLine("Введите первое число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (Math.Pow(num1, 2) == num2 || Math.Pow(num2, 2) == num1)
+{ Console.WriteLine("Одно число является квадратом другого."); }
+else
+{ Console.WriteLine("Эти числа не являются квадратами друг друга."); }
+*/
+//
+/*
+int MaxNum(int num)
+{
+    int num1 = num % 10;
+    int num2 = num / 10;
+    if (num1 > num2) return num1;
+    else return num2;
+}
+int number = new Random().Next(10, 100);
+Console.WriteLine(number);
+int res = MaxNum(number);
+Console.WriteLine(res);
+*/
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int x = Convert.ToInt32(Console.ReadLine());
+        // int y = Convert.ToInt32(Console.ReadLine());
+        Fact(x);
+    }
+
+    private static void Fact(int x) //Считаем методом
+    {
+        // int result = Convert.ToInt32((x * y) + Math.Pow(x, 2));
+        int n = 1;
+        for (int i = 2; i <= x; i++)
+        {
+            n *= i;
+        }
+        Print(n);
+        // Print(result);
+    }
+
+    private static void Print(int x) //Печатаем методом
+    {
+        Console.WriteLine("Факториал {0} равен {1}", x, n);
+    }
+}
